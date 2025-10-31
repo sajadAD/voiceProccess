d948f3e29555eed320b38e0f30930e9316abc80c
sajadeydi <sajadeydi8026@gmail.com>
2025-10-31 13:20:34 +0330

    handle zcr

--- gui.py
+++ gui.py
@@ -3,13 +3,14 @@ from tkinter import filedialog, messagebox
 from histogram import plot_histogram
 from energy import plot_energy
 from vad import plot_vad
+from zcr import plot_zcr
 
 class AudioApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Audio Analyzer")
-        self.root.geometry("400x300")
-        self.root.resizable(False, False)
+        self.root.geometry("800x800")
+        self.root.resizable(True, True)
 
         self.file_path = None
         self.setup_ui()
@@ -37,9 +38,15 @@ class AudioApp:
                   command=self.show_vad, bg="#9C27B0",
                   fg="white", width=20).pack(pady=5)
         
+        tk.Button(self.root, text="Show ZCR",
+                  command=self.show_zcr, bg="#673AB7",
+                  fg="white", width=20).pack(pady=5)
+        
         tk.Button(self.root, text="Exit",
                   command=self.root.destroy, bg="#f44336",
                   fg="white", width=20).pack(pady=10)
+        
+
 
     def select_file(self):
         file = filedialog.askopenfilename(
@@ -76,5 +83,14 @@ class AudioApp:
             return
         try:
             plot_vad(self.file_path)
+        except Exception as e:
+            messagebox.showerror("Error", f"Something went wrong:\n{e}")
+
+    def show_zcr(self):
+        if not self.file_path:
+            messagebox.showerror("Error", "Please select a WAV file first.")
+            return
+        try:
+            plot_zcr(self.file_path)
         except Exception as e:
             messagebox.showerror("Error", f"Something went wrong:\n{e}")
